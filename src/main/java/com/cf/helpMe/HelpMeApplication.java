b2b381aa986b0e8dce0e2ab4e08c8a3558e29479
FranckNana <nanafranck94@gmail.com>
2021-07-09 21:25:18 +0200

    ajout table preferences

--- src/main/java/com/cf/helpMe/HelpMeApplication.java
+++ src/main/java/com/cf/helpMe/HelpMeApplication.java
@@ -2,11 +2,14 @@ package com.cf.helpMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class HelpMeApplication {
 
 	public static void main(String[] args) {
--- src/main/resources/db/changelog/changes/insert-table-changelog-2.sql
+++ src/main/resources/db/changelog/changes/insert-table-changelog-2.sql
@@ -1,2 +1,11 @@
-INSERT INTO users ("email", "password", username) VALUES('judicaelnana94@gmail.com','a123b456', 'judicael');
-INSERT INTO users ("email", "password", username) VALUES('cephaszoubga@gmail.com','a123b456', 'zcephas');
+INSERT INTO users ("email", "password", username) VALUES('judicaelnana94@gmail.com','$2a$10$tn1tngJuOBY/LIet2xYcwuUHSxFAIpTqoWwHHzXXsBLcoichVodLa', 'judicael');
+INSERT INTO users ("email", "password", username) VALUES('cephaszoubga@gmail.com','$2a$10$tn1tngJuOBY/LIet2xYcwuUHSxFAIpTqoWwHHzXXsBLcoichVodLa', 'zcephas');
+
+INSERT INTO preferences("matiere") VALUES ('MATH');
+INSERT INTO preferences("matiere") VALUES ('PC');
+INSERT INTO preferences("matiere") VALUES ('SVT');
+INSERT INTO preferences("matiere") VALUES ('FR');
+INSERT INTO preferences("matiere") VALUES ('ANG');
+INSERT INTO preferences("matiere") VALUES ('ALL');
+INSERT INTO preferences("matiere") VALUES ('PHILOS');
+INSERT INTO preferences("matiere") VALUES ('HG');
