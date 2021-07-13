90ef15796e5424d87cb9ec92a224f8e084286a3c
FranckNana <nanafranck94@gmail.com>
2021-07-13 11:23:58 +0200

    ajout de fichier de config

--- src/main/java/com/cf/helpMe/HelpMeApplication.java
+++ src/main/java/com/cf/helpMe/HelpMeApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableConfigurationProperties({LiquibaseProperties.class})
+@PropertySource(value = {"classpath:application.prod.properties"},ignoreResourceNotFound = true)
 public class HelpMeApplication {
 
 	public static void main(String[] args) {
